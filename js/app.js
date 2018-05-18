901d860847ba9973e3f0295caa357c253a8898fa
MartinKoeoep <herrkoop@gmail.com>
2018-05-18 15:29:18 +0100

    Reworked movement so it's not redrawing the map each turn. Only updates the classes changed during a move.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,37 @@
-let gameGrid = [
+const gameGrid = [
   [0,0,0,0,0,0,0,0,0,0],
-  [0,0,1,0,1,0,0,0,0,0],
-  [0,1,1,9,1,1,1,1,1,0],
+  [0,0,1,9,1,0,0,0,0,0],
+  [0,1,1,0,1,1,1,1,1,0],
   [0,1,0,0,1,0,0,0,1,0],
   [0,1,0,0,1,0,0,0,1,0],
   [0,1,0,0,0,0,0,0,1,0],
   [0,1,1,1,1,1,0,1,1,0],
   [0,0,0,0,0,1,0,1,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,1,0,1,0,0],
   [0,0,0,0,0,0,0,0,0,0]
 ];
 //  0 = floor; 1 = wall; .. 9= playerCharacter
 
 const $playerCharacter = $('#playerCharacter');
-let playerLocation = [];
-const $map = $('div#map');
+let playerLocation = {};
+const $map = $('#map');
 
 $(() => {
+
+  function emptyCheck() {
+    $map.empty();
+  }
+
+  $('.tester').on('click', () => {
+    emptyCheck();
+  });
 
   $('#map').on('mouseover','div', function() {
     $('#cell-address').val(`${$(this).data('x')}-${$(this).data('y')}`);
   });
   function drawMap(){
-
-    $map.innerHTML = '';
+    console.log($map);
+    // $map.empty();
 
     $.each(gameGrid, (i,row) => {
       $.each(row, (j,cell) => {
@@ -34,56 +42,57 @@ $(() => {
           $element.addClass('wall');
         } else if (cell === 9) {
           $element.addClass('playerCharacter');
-          playerLocation = [i, j];
+          playerLocation = {
+            x: i,
+            y: j
+          };
         }
-        $element.data({x: i, y: j});
+        $element.attr('data-x', i);
+        $element.attr('data-y', j);
         $element.on('click', function(){
           console.log($(this));
         });
-        $element.appendTo('#map');
+        $element.appendTo('#map').clone(true);
       });
     });
+
   }
 
   function movePlayer(){
     $(document).on('keypress', function(e){
       switch(e.which){
         case 119:
-          console.log('up');
-          if (gameGrid[playerLocation[0]-1][playerLocation[1]] === 0){
-            gameGrid[playerLocation[0]][playerLocation[1]] = 0;
-            gameGrid[playerLocation[0]-1][playerLocation[1]] = 9;
-            drawMap();
+          if (gameGrid[playerLocation.x-1][playerLocation.y] === 0){
+            $('.playerCharacter').removeClass('playerCharacter').addClass('floor');
+            playerLocation.x -= 1;
+            $(`div[data-x='${playerLocation.x}'][data-y='${playerLocation.y}']`).removeClass('floor').addClass('playerCharacter');
           } else {
             console.log('Cannot move in that direction');
           }
           break;
         case 97:
-          console.log('left');
-          if (gameGrid[playerLocation[0]][playerLocation[1]-1] === 0){
-            gameGrid[playerLocation[0]][playerLocation[1]] = 0;
-            gameGrid[playerLocation[0]][playerLocation[1]-1] = 9;
-            drawMap();
+          if (gameGrid[playerLocation.x][playerLocation.y-1] === 0){
+            $('.playerCharacter').removeClass('playerCharacter').addClass('floor');
+            playerLocation.y -= 1;
+            $(`div[data-x='${playerLocation.x}'][data-y='${playerLocation.y}']`).removeClass('floor').addClass('playerCharacter');
           } else {
             console.log('Cannot move in that direction');
           }
           break;
         case 115:
-          console.log('down');
-          if (gameGrid[playerLocation[0]+1][playerLocation[1]] === 0){
-            gameGrid[playerLocation[0]][playerLocation[1]] = 0;
-            gameGrid[playerLocation[0]+1][playerLocation[1]] = 9;
-            drawMap();
+          if (gameGrid[playerLocation.x+1][playerLocation.y] === 0){
+            $('.playerCharacter').removeClass('playerCharacter').addClass('floor');
+            playerLocation.x += 1;
+            $(`div[data-x='${playerLocation.x}'][data-y='${playerLocation.y}']`).removeClass('floor').addClass('playerCharacter');
           } else {
             console.log('Cannot move in that direction');
           }
           break;
         case 100:
-          console.log('right');
-          if (gameGrid[playerLocation[0]][playerLocation[1]+1] === 0){
-            gameGrid[playerLocation[0]][playerLocation[1]] = 0;
-            gameGrid[playerLocation[0]][playerLocation[1]+1] = 9;
-            drawMap();
+          if (gameGrid[playerLocation.x][playerLocation.y+1] === 0){
+            $('.playerCharacter').removeClass('playerCharacter').addClass('floor');
+            playerLocation.y += 1;
+            $(`div[data-x='${playerLocation.x}'][data-y='${playerLocation.y}']`).removeClass('floor').addClass('playerCharacter');
           } else {
             console.log('Cannot move in that direction');
           }
